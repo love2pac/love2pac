09199d14d26aae0f9f54cf0ce777ccd6323d2944
love2pac <78852686+love2pac@users.noreply.github.com>
2021-02-11 16:19:03 +0800

    Add files via upload

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * 声明，本项目仅帮助大家学习技术及娱乐，切勿将修改后的网站大规模传播及商用，以避免侵权！
  */
 
@@ -35,10 +35,10 @@ let fruitSlowDown = false;
 let clickChangeFruit = true;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+let adLink = 'false';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成兔叭咯';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
